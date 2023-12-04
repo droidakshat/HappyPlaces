f892c53f9528bc67cffb740411ec7e61d551b510
akshat gupta <akshatguptaak47@gmail.com>
2023-12-04 15:15:45 +0530

    initial commit

---
+++ app/src/main/java/com/example/happyplaces/AddHappyPlaceActivity.kt
@@ -0,0 +1,18 @@
+package com.example.happyplaces
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toolbar
+
+class AddHappyPlaceActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_happy_place)
+        val toolBarAddHappyPlace:androidx.appcompat.widget.Toolbar=findViewById(R.id.toolBarAddHappyPlaces)
+        setSupportActionBar(toolBarAddHappyPlace)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        toolBarAddHappyPlace.setNavigationOnClickListener {
+            onBackPressed()
+        }
+    }
+}
--- app/src/main/java/com/example/happyplaces/MainActivity.kt
+++ app/src/main/java/com/example/happyplaces/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.example.happyplaces
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        var floatingbutton:FloatingActionButton=findViewById(R.id.fabAddHappyPlace)
+        floatingbutton.setOnClickListener {
+            val intent= Intent(this,AddHappyPlaceActivity::class.java)
+            startActivity(intent)
+        }
+
+
     }
 }
